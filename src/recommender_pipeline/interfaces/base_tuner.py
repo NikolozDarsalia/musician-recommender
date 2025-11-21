55905dcca83b4ec840b33f51d5f94343b0267426
NikolozDarsalia <nikadarsalia@gmail.com>
2025-11-21 23:21:11 +0100

    adjustments in interfaces (mostly about comments)

--- src/recommender_pipeline/interfaces/base_tuner.py
+++ src/recommender_pipeline/interfaces/base_tuner.py
@@ -1,8 +1,14 @@
 from abc import ABC, abstractmethod
 
-class BaseTuner(ABC):
 
+class BaseTuner(ABC):
     @abstractmethod
-    def optimize(self, train, val, user_features, item_features):
-        """Returns best hyperparameters."""
+    def optimize(self, train, val, user_features, item_features, n_trials, direction):
+        """
+        train: training interactions
+        val: validation interactions
+        user_features, item_features: sparse matrices
+        n_trials: number of Optuna trials
+        direction: maximize or minimize
+        """
         raise NotImplementedError
