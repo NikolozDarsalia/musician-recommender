828fd8f2a156b897625c2f0b7972f311be4a2d8b
NikolozDarsalia <nikadarsalia@gmail.com>
2025-11-22 15:19:03 +0100

    added train_test_split and folder_path parameter in loader interface

--- src/recommender_pipeline/interfaces/base_loader.py
+++ src/recommender_pipeline/interfaces/base_loader.py
@@ -1,9 +1,16 @@
 import pandas as pd
 from abc import ABC, abstractmethod
+from types import Tuple
+
 
 class BaseLoader(ABC):
+    @abstractmethod
+    def load(self, folder_path: str) -> pd.DataFrame:
+        """Loads the dataset and returns a pandas DataFrame."""
+        raise NotImplementedError
 
     @abstractmethod
-    def load(self) -> pd.DataFrame:
-        """Loads the dataset and returns a pandas DataFrame."""
+    def train_test_split(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Train-test split for interaction's dataframe
+        VALID ONLY FOR INTERACTION'S DATAFRAME!"""
         raise NotImplementedError
