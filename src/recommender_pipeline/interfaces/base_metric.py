723dcecfecbfa5254dcbd899cf893e090b0d39d8
NikolozDarsalia <nikadarsalia@gmail.com>
2025-11-22 15:20:06 +0100

    changed base_metric interface, added comments and name function

--- src/recommender_pipeline/interfaces/base_metric.py
+++ src/recommender_pipeline/interfaces/base_metric.py
@@ -3,5 +3,15 @@ from abc import ABC, abstractmethod
 
 class BaseMetric(ABC):
     @abstractmethod
-    def compute(self, model, interactions_df, k: int):
+    def compute(self, model, interactions_df, user_features=None, item_features=None):
+        """
+        Compute the metric on the given interactions matrix.
+        Returns a float.
+        """
         raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def name(self):
+        """Return the metric name."""
+        pass
