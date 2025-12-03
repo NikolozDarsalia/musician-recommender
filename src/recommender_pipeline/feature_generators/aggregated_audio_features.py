39eeb05afcf4eaf7073701b265e764b9f5b950ef
NikolozDarsalia <nikadarsalia@gmail.com>
2025-12-03 23:07:50 +0100

    added aggregated_audio_features skeleton

---
+++ src/recommender_pipeline/feature_generators/aggregated_audio_features.py
@@ -0,0 +1,5 @@
+from interfaces.base_feature_generator import BaseFeatureGenerator
+
+
+class AggregatedAudioGenerator(BaseFeatureGenerator):
+    """Generates aggregated audio features from Spotify Dataset"""
