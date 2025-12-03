91442fe319c7ecb3a030c6a1a4412e08f2a6c1e0
NikolozDarsalia <nikadarsalia@gmail.com>
2025-12-03 23:07:27 +0100

    deleted not necessary skeletons

--- src/recommender_pipeline/feature_generators/user_temporal_features.py
+++
@@ -1,5 +0,0 @@
-from interfaces.base_feature_generator import BaseFeatureGenerator
-
-
-class UserTemporalFeatureGenerator(BaseFeatureGenerator):
-    """Creates user time-of-day/week activity vectors."""
